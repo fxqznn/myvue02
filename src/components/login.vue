722690e1d6a3c51ead160c12e18cd83b361f4436
wk <1037128119@qq.com>
2020-11-05 15:57:44 +0800

    wk

--- src/components/login.vue
+++ src/components/login.vue
@@ -86,6 +86,8 @@
 
                 }else if(this.$store.state.user.role == 2) {
 
+                  this.$router.push({path: "/mainFrame"})
+
                 } else if (res.data.role==3){
                   axios.get("getByUser/"+this.$store.state.user.uid).then(res =>{
                     this.$store.dispatch("student/setStudent",res.data);
--- src/components/managerEdit.vue
+++ src/components/managerEdit.vue
@@ -1,82 +1,277 @@
 <template>
-  <div>
-    <table >
-      <tr>
-        <td colspan="10"> <h2>金桥学员成长跟踪表</h2></td>
-      </tr>
-      <tr >
-        <td height="40px" >姓名</td>
-        <td >{{student.sname}}</td>
-        <td>性别</td>
-        <td>{{student.sex}}</td>
-        <td colspan="2">民族</td>
-        <td colspan="2">{{student.nation}}</td>
-        <td rowspan="4" colspan="2"><img :src=student.pic></td>
-      </tr>
-      <tr>
-        <td height="40px">出生年月</td>
-        <td>{{student.birthday}}</td>
-        <td >籍贯</td>
-        <td >{{student.address}}</td>
-        <td colspan="2">婚否</td>
-        <td colspan="2"><span>{{student.marry ==0? "未婚":"已婚"}}</span></td>
-      </tr>
-      <tr>
-        <td height="40px">联系电话</td>
-        <td colspan="2">{{student.phone}}</td>
-        <td>身份证号码</td>
-        <td colspan="4">{{student.cardid}}</td>
-      </tr>
-      <tr>
-        <td height="40px">毕业学校</td>
-        <td colspan="2">{{student.school}}</td>
-        <td>专业</td>
-        <td colspan="4" >{{student.major}}</td>
-      </tr>
-      <tr>
-        <td height="40px">备注</td>
-        <td colspan="9">{{student.info}}</td>
-      </tr>
-      <tr>
-        <td colspan="10" height="40px">
-          <h3>{{type == 0 ? "转正工作评价": "第一年工作评价"}}</h3>
-        </td>
-      </tr>
-      <tr>
-        <td height="55px" rowspan="2">项目</td>
-        <td rowspan="2">员工部门</td>
-        <td rowspan="2">员工职务</td>
-        <td rowspan="2">评价人</td>
-        <td colspan="5" height="20">评价分项</td>
-        <td width="120" rowspan="2">整体评价分数</td>
-      </tr>
-      <tr>
 
-        <td width="50">能力</td>
-        <td width="50">交流</td>
-        <td width="50">积极性</td>
-        <td width="50">性格</td>
-        <td width="50">人品</td>
+    <div>
+      <div>
 
-      </tr>
-      <tr>
-        <td height="40px">工作评价</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td width="60px" height="150px">评价(包括主要优点及缺陷)</td>
-        <td colspan="9"></td>
-      </tr>
-    </table>
-  </div>
+      </div>
+      <table width="80%">
+        <tr>
+          <td colspan="10"> <h2>金桥学员成长跟踪表</h2></td>
+        </tr>
+        <tr >
+          <td height="40px" >姓名</td>
+          <td >{{student.sname}}</td>
+          <td>性别</td>
+          <td>{{student.sex}}</td>
+          <td colspan="2">民族</td>
+          <td colspan="2">{{student.nation}}</td>
+          <td rowspan="4" colspan="2"><img :src=student.pic></td>
+        </tr>
+        <tr>
+          <td height="40px">出生年月</td>
+          <td>{{student.birthday}}</td>
+          <td >籍贯</td>
+          <td >{{student.address}}</td>
+          <td colspan="2">婚否</td>
+          <td colspan="2"><span>{{student.marry ==0? "未婚":"已婚"}}</span></td>
+        </tr>
+        <tr>
+          <td height="40px">联系电话</td>
+          <td colspan="2">{{student.phone}}</td>
+          <td>身份证号码</td>
+          <td colspan="4">{{student.cardid}}</td>
+        </tr>
+        <tr>
+          <td height="40px">毕业学校</td>
+          <td colspan="2">{{student.school}}</td>
+          <td>专业</td>
+          <td colspan="4" >{{student.major}}</td>
+        </tr>
+        <tr>
+          <td height="40px">备注</td>
+          <td colspan="9">{{student.info}}</td>
+        </tr>
+        <tr>
+          <td colspan="10" height="40px">
+            <h3>转正工作评价</h3>
+          </td>
+        </tr>
+        <tr>
+          <td height="55px" rowspan="2">项目</td>
+          <td rowspan="2">员工部门</td>
+          <td rowspan="2" width="60px">员工职务</td>
+          <td rowspan="2">评价人</td>
+          <td colspan="5" height="20">评价分项</td>
+          <td width="20" rowspan="2">整体评价分数</td>
+        </tr>
+        <tr>
+
+          <td width="60">能力</td>
+          <td width="60">交流</td>
+          <td width="60">积极性</td>
+          <td width="60">性格</td>
+          <td width="60">人品</td>
+
+        </tr>
+        <tr>
+          <td height="40px">工作评价</td>
+          <td>{{emp.dname}}</td>
+          <td>{{emp.job}}</td>
+          <td>{{ename1}}</td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1" ></el-input>
+          </td>
+        </tr>
+        <tr>
+          <td width="60px" >评价(包括主要优点及缺陷)</td>
+          <td colspan="9">
+            <el-input
+              type="textarea"
+              :rows="4"
+              v-model="textarea">
+            </el-input>
+          </td>
+        </tr>
+
+        <tr>
+          <td colspan="10" height="40px">
+            <h3>第一年工作评价</h3>
+          </td>
+        </tr>
+        <tr>
+          <td height="55px" rowspan="2">项目</td>
+          <td rowspan="2">员工部门</td>
+          <td rowspan="2" width="60px">员工职务</td>
+          <td rowspan="2">评价人</td>
+          <td colspan="5" height="20">评价分项</td>
+          <td width="120" rowspan="2">整体评价分数</td>
+        </tr>
+        <tr>
+
+          <td width="60">能力</td>
+          <td width="60">交流</td>
+          <td width="60">积极性</td>
+          <td width="60">性格</td>
+          <td width="60">人品</td>
+
+        </tr>
+        <tr>
+          <td height="40px">工作评价</td>
+          <td>{{emp.dname}}</td>
+          <td>{{emp.job}}</td>
+          <td>{{ename1}}</td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1" ></el-input>
+          </td>
+        </tr>
+        <tr>
+          <td width="60px" >评价(包括主要优点及缺陷)</td>
+          <td colspan="9">
+            <el-input
+              type="textarea"
+              :rows="4"
+              v-model="textarea">
+            </el-input>
+          </td>
+        </tr>
+
+        <tr>
+          <td colspan="10" height="40px">
+            <h3>第二年工作评价</h3>
+          </td>
+        </tr>
+        <tr>
+          <td height="55px" rowspan="2">项目</td>
+          <td rowspan="2">员工部门</td>
+          <td rowspan="2" width="60px">员工职务</td>
+          <td rowspan="2">评价人</td>
+          <td colspan="5" height="20">评价分项</td>
+          <td width="120" rowspan="2">整体评价分数</td>
+        </tr>
+        <tr>
+
+          <td width="60">能力</td>
+          <td width="60">交流</td>
+          <td width="60">积极性</td>
+          <td width="60">性格</td>
+          <td width="60">人品</td>
+
+        </tr>
+        <tr>
+          <td height="40px">工作评价</td>
+          <td>{{emp.dname}}</td>
+          <td>{{emp.job}}</td>
+          <td>{{ename1}}</td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1" ></el-input>
+          </td>
+        </tr>
+        <tr>
+          <td width="60px" >评价(包括主要优点及缺陷)</td>
+          <td colspan="9">
+            <el-input
+              type="textarea"
+              :rows="4"
+              v-model="textarea">
+            </el-input>
+          </td>
+        </tr>
+
+        <tr>
+          <td colspan="10" height="40px">
+            <h3>第三年工作评价</h3>
+          </td>
+        </tr>
+        <tr>
+          <td height="55px" rowspan="2">项目</td>
+          <td rowspan="2">员工部门</td>
+          <td rowspan="2" width="60px">员工职务</td>
+          <td rowspan="2">评价人</td>
+          <td colspan="5" height="20">评价分项</td>
+          <td width="120" rowspan="2">整体评价分数</td>
+        </tr>
+        <tr>
+
+          <td width="60">能力</td>
+          <td width="60">交流</td>
+          <td width="60">积极性</td>
+          <td width="60">性格</td>
+          <td width="60">人品</td>
+
+        </tr>
+        <tr>
+          <td height="40px">工作评价</td>
+          <td>{{emp.dname}}</td>
+          <td>{{emp.job}}</td>
+          <td>{{ename1}}</td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1"></el-input>
+          </td>
+          <td>
+            <el-input v-model="score1" ></el-input>
+          </td>
+        </tr>
+        <tr>
+          <td width="60px" >评价(包括主要优点及缺陷)</td>
+          <td colspan="9">
+            <el-input
+              type="textarea"
+              :rows="4"
+              v-model="textarea">
+            </el-input>
+          </td>
+        </tr>
+      </table>
+    </div>
+
+
 
 </template>
 
@@ -100,9 +295,17 @@
               school:"",
               major:'',
               pic:"",
-              info:""
+              info:"",
             },
+            emp:{
+              ename:"",
+              dname:"",
+              job:"",
+            },
+            ename1:"",
             sid:0,
+            score1:"",
+            textarea:"",
           }
       },
       methods:{
@@ -110,13 +313,19 @@
             axios.get("http://localhost:8081/getStudent?eid="+this.eid).then(res=>{
               this.student =res.data;
             })
+          },
+          getEmp(){
+            axios.get("http://localhost:8081/getManager?eid="+this.eid).then(res=>{
+              this.emp = res.data;
+            })
           }
       },
       mounted() {
-          this.eid = this.$route.params.eid;
+            this.eid = this.$route.params.eid;
+            this.ename1 = this.$route.params.ename;
             this.showInfo();
+            this.getEmp();
             this.getAllScores();
-
 
       }
     }
@@ -132,4 +341,5 @@
     border: solid 1px black;
     border-collapse: collapse;
   }
+
 </style>
